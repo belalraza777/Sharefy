965c8f6255080e76769c3ef99830bd5a88708520
belal raza <belalraza707@gmail.com>
2026-02-01 13:02:28 +0530

    fix

--- Frontend/src/routes/AppRoute.jsx
+++ Frontend/src/routes/AppRoute.jsx
@@ -6,12 +6,12 @@ import ScrollToTop from './ScrollToTop';
 import Skeleton from '../components/Skeleton/Skeleton';
 import OAuthSuccess from '../pages/oauth/oAuth-success';
 import Feed from '../pages/Feed/Feed';
+import Profile from '../pages/User_Profile/Profile';
 
 import LoginPage from '../pages/Auth/login';
 const SignupPage = lazy(() => import('../pages/Auth/signup'));
 const SinglePost = lazy(() => import('../pages/Post/SinglePost'));
 const CreatePost = lazy(() => import('../pages/New_Post/CreatePost'));
-const Profile = lazy(() => import('../pages/User_Profile/Profile'));
 const Notification = lazy(() => import('../pages/Notification/Notification'));
 const Settings = lazy(() => import('../pages/Settings/Settings'));
 const Explore = lazy(() => import('../pages/Explore/Explore'));
